1565caa12ba81c2a19880f0139960f9ad9e91272
Jo ye hyeon <sjkang@daegu.ac.kr>
2023-04-04 23:49:19 +0900

    conflict resolved

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -2,13 +2,11 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		//TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("Hello his world");
-<<<<<<< HEAD
 		System.out.println("Hello your world");
-=======
 		System.out.println("Hello her world");
->>>>>>> refs/heads/hotfix
 
 	}
 
