be4813c7d1a94e2ca22234d36e8d6223aa68108a
yehyeon6 <129235921+yehyeon6@users.noreply.github.com>
2023-04-05 09:43:34 +0900

    Pushprint.java modifed on Github

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -3,7 +3,7 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		System.out.println("Add Java file");
-		System.out.println("Hello...")
+		System.out.println("Hello...");
 
 	}
 
